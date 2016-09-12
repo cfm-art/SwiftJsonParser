74e660b1187e4fd59fc46fc0a5245a530c86ee07
art <mail@cfm-art.sakura.ne.jp>
2016-09-12 19:33:49 +0900

    Initial Commit

---
+++ SwiftJsonParser/SwiftJsonParser.h
@@ -0,0 +1,19 @@
+//
+//  SwiftJsonParser.h
+//  SwiftJsonParser
+//
+//  Created by あると on 2016/09/12.
+//  Copyright © 2016年 あると. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftJsonParser.
+FOUNDATION_EXPORT double SwiftJsonParserVersionNumber;
+
+//! Project version string for SwiftJsonParser.
+FOUNDATION_EXPORT const unsigned char SwiftJsonParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftJsonParser/PublicHeader.h>
+
+
